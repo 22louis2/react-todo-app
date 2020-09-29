3bb3218014b01c8e98e47632210f152da8c5ba58
22louis2 <otulouisakhigbe@gmail.com>
2020-09-29 02:37:08 +0100

    Updates

---
+++ precache-manifest.b7f5aa3bf31677f7e0bb0801aa5d2f40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1905496c579fa8b922ac9c51a1d1bc6a",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "9814c9366c3cc66d59f0",
+    "url": "/react-todo-app/static/css/main.c022ac42.chunk.css"
+  },
+  {
+    "revision": "30b9df83b291919ab0e0",
+    "url": "/react-todo-app/static/js/2.dc9f8f68.chunk.js"
+  },
+  {
+    "revision": "9814c9366c3cc66d59f0",
+    "url": "/react-todo-app/static/js/main.7443f08e.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
